d4229e1738c46df488f905cfd722430a9c9694d2
m-oline <marieolineobergfaurhoj@gmail.com>
2025-12-02 19:31:50 +0100

    Update dmi.R

--- dmi.R
+++ dmi.R
@@ -13,9 +13,25 @@ df_long <- pivot_longer(
 
 ggplot(df_long, aes(x = Dato, y = Value, color = Type)) +
   geom_line() +
+  geom_point(shape = 21, fill = "transparent", size = 3, stroke = 1) +
   scale_x_datetime(
     breaks = seq(min(df_long$Dato), max(df_long$Dato), by = "2 days"),
     date_labels = "%Y-%m-%d"
-  )
+  ) +
+  scale_y_continuous(
+    breaks = seq(-5, max(df_long$Value, na.rm = TRUE), by = 5)
+  ) +
+  scale_color_manual(
+    values = c(
+      "Middel" = "green",
+      "Laveste" = "blue",
+      "Højeste" = "red"
+    )
+  ) +
+  labs(
+    y = "Temperatur",
+    x = "Dato"
+  ) +
+  theme_minimal()
 
-ggsave("temperature_plot.png", width = 10, height = 6, dpi = 300)
+ggsave("temperature_plot.png", width = 25, height = 6, dpi = 300)
