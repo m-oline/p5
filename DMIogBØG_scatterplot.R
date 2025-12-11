cc6d4e8861eac3969bcc07fb45b4325c8a678b72
m-oline <marieolineobergfaurhoj@gmail.com>
2025-12-11 10:45:01 +0100

    scatterplot for DMI and BØG

---
+++ DMIogBØG_scatterplot.R
@@ -0,0 +1,50 @@
+source("./load_tree_data.R")
+source("./load_dmi_data.R")
+
+library(dplyr)
+library(ggplot2)
+
+dmi <- load_dmi_data(file_name = "dmi-data.csv") %>%
+  filter(Type == "Middel") %>%
+  select(Dato, Value) %>%
+  mutate(group = "dmi")
+
+
+bøg <- load_tree_data("golfpark-bøg", long = TRUE) %>%
+  filter(Type == "Middel") %>%
+  select(Dato, Value) %>%
+  mutate(group = "bøg")
+
+dat <- bind_rows(dmi, bøg)
+
+dat_avg <- dat %>%
+  group_by(Dato, group) %>%
+  summarise(
+    Value = mean(Value, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+ggplot() +
+  geom_point(
+    data = dplyr::filter(dat_avg, group == "dmi"),
+    aes(x = Dato, y = Value),
+    color = "green"
+  ) +
+  geom_point(
+    data = dplyr::filter(dat_avg, group == "bøg"),
+    aes(x = Dato, y = Value),
+    color = "red"
+  ) +
+  geom_smooth(
+    data = dat_avg,
+    aes(x = Dato, y = Value),
+    method = "lm",
+    se = FALSE,
+    color = "black"
+  ) +
+  labs(
+    x = "Dato",
+    y = "Temperatur (°C)",
+    title = "Temperaturer fra DMI (grøn) og Bøg (rød) over tid"
+  ) +
+  theme_minimal()
